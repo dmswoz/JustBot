4a992df0e9dcdd6ff284137da5a82ec010fa8f65
helikespuppy <lej10048207@outlook.kr>
2024-02-08 22:17:17 +0900

    lint:fix

--- src/classes/JustClient.ts
+++ src/classes/JustClient.ts
@@ -46,34 +46,58 @@ export class JustClient extends SapphireClient {
         message.reply(`> ⚠️ 요청 거부됨. 권한을 확인하세요.`)
     });
 
-    this.audio = new Kazagumo({
-      defaultSearchEngine: "youtube",
-      send: (guildId, payload) => {
-        const guild = this.guilds.cache.get(guildId);
-        if(guild) guild.shard.send(payload);
-      }
-    }, new Connectors.DiscordJS(this), [{
-      name: process.env.LAVA_NAME!,
-      url: `${process.env.LAVA_HOST}:${process.env.LAVA_PORT}`,
-      auth: process.env.LAVA_AUTH!,
-      secure: false
-    }]);
+    this.audio = new Kazagumo(
+      {
+        defaultSearchEngine: "youtube",
+        send: (guildId, payload) => {
+          const guild = this.guilds.cache.get(guildId);
+          if (guild) guild.shard.send(payload);
+        }
+      },
+      new Connectors.DiscordJS(this),
+      [
+        {
+          name: process.env.LAVA_NAME!,
+          url: `${process.env.LAVA_HOST}:${process.env.LAVA_PORT}`,
+          auth: process.env.LAVA_AUTH!,
+          secure: false
+        }
+      ]
+    );
 
-    this.audio.shoukaku.on("ready", (name, resumed) => this.logger.info(`[Plugin#Audio:Ready] Lavalink Node: ${name} is now connected. This connection is ${resumed ? "resumed" : "a new connection"}.`));
-    this.audio.shoukaku.on("error", (name, error) => this.logger.error(`[Plugin#Audio:Error] Lavalink Node: ${name} emitted an error. ${error.stack}`));
-    this.audio.shoukaku.on("close", (name, code, reason) => this.logger.warn(`[Plugin#Audio:Warn] Lavalink Node: ${name} closed with code ${code}. Reason: ${reason || "No reason."}`));
+    this.audio.shoukaku.on("ready", (name, resumed) =>
+      this.logger.info(
+        `[Plugin#Audio:Ready] Lavalink Node: ${name} is now connected. This connection is ${resumed ? "resumed" : "a new connection"}.`
+      )
+    );
+    this.audio.shoukaku.on("error", (name, error) =>
+      this.logger.error(
+        `[Plugin#Audio:Error] Lavalink Node: ${name} emitted an error. ${error.stack}`
+      )
+    );
+    this.audio.shoukaku.on("close", (name, code, reason) =>
+      this.logger.warn(
+        `[Plugin#Audio:Warn] Lavalink Node: ${name} closed with code ${code}. Reason: ${reason || "No reason."}`
+      )
+    );
     // this.audio.shoukaku.on("disconnect", (name, players, moved) => {
     //   if(moved) return;
     //   players.map((player) => player.connection.disconnect());
     //   this.logger.warn(`[Plugin#Audio:Warn] Lavalink Node: ${name} disconnected.`);
     // });
-    this.audio.shoukaku.on("debug", (name, data) => this.logger.debug(`[Plugin#Audio:Debug] Lavalink Node: ${name} - Data: ${JSON.stringify(data)}`));
+    this.audio.shoukaku.on("debug", (name, data) =>
+      this.logger.debug(
+        `[Plugin#Audio:Debug] Lavalink Node: ${name} - Data: ${JSON.stringify(data)}`
+      )
+    );
 
-    this.audio.on("playerStart", (player, track) => {
-      (this.channels.cache.get(player.textId!) as TextChannel).send({
-        content: `>`
-      }).then((x) => player.data.set("message", x));
-    });
+    // this.audio.on("playerStart", (player, track) => {
+    //   (this.channels.cache.get(player.textId!) as TextChannel)
+    //     .send({
+    //       content: `>`
+    //     })
+    //     .then((x) => player.data.set("message", x));
+    // });
   }
 
   public async start() {
