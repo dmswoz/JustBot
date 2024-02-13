6aa3e6774da6361c64f8e950f7b13935bbffb991
helikespuppy <lej10048207@outlook.kr>
2024-02-13 13:58:31 +0900

    add(listener): error

---
+++ src/listeners/Client/Error.ts
@@ -0,0 +1,16 @@
+import { Listener } from "@sapphire/framework";
+import * as Sentry from "@sentry/node";
+
+export class Error extends Listener {
+    public constructor(context: Listener.LoaderContext, options: Listener.Options) {
+        super(context, {
+            ...options,
+            event: "error"
+        });
+    }
+
+    public run(error: Error) {
+        this.container.logger.error(`[JustBot:Error] ${error}`);
+        Sentry.captureException(error);
+    }
+}
